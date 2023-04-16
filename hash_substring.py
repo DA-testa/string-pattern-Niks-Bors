aa06e6a0a809ff7863b7cff60199964803d5dfc5
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-04-16 14:45:52 +0000

    =

--- hash_substring.py
+++ hash_substring.py
@@ -3,17 +3,21 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    input_type = input().strip()
-    pattern, text = None, None
+    rinda = input()
     
-    if input_type == "F":
-        file_name = input().strip()
+    
+    if rinda == "F":
+        fileName="06"
+        file_name = "./tests/" + fileName
         with open(file_name, 'r') as f:
             pattern = f.readline().strip()
             text = f.readline().strip()
-    elif input_type == "I":
+            return (text, pattern)
+
+    elif rinda == "I":
         pattern = input().strip()
         text = input().strip()
+        return (text, pattern)
     
         
     else:
@@ -29,7 +33,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (text, pattern)
+    
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
