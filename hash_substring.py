db33628c79a233e56b6601c125676b98460121b1
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-04-16 16:25:15 +0000

    =

--- hash_substring.py
+++ hash_substring.py
@@ -80,7 +80,7 @@ def get_occurrences(pattern, text):
 
 
 # this part launches the functions
-if name == 'main':
+if __name__ == '__main__':
     print_occurrences(get_occurrences(read_input()))
    
 
