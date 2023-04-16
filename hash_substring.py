505aedf4b301517b45b06d11f1def383156ab554
Niks Bors <124885258+Niks-Bors@users.noreply.github.com>
2023-04-16 14:39:49 +0000

    =

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,7 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-     input_type = input().strip()
+    input_type = input().strip()
     pattern, text = None, None
     
     if input_type == "F":
